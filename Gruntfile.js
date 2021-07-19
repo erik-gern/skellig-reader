10087557d0c93131a1486bcef4e803a862757410
Erik Gern <erik.gern@gmail.com>
2021-07-19 09:49:54 -0400

    moved tasks from package.json to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+const sass = require('sass');
+
+module.exports = function(grunt){
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		clean: ['build/*', '!build/.gitignore'],
+		copy: {
+			main: {
+				files: [ { expand: true, cwd: 'static', src: '**', dest: 'build/', } ],
+			},
+		},
+		sass: {
+			options: {
+				implementation: sass,
+				sourceMap: true,
+			},
+			dist: {
+				files: {
+					'build/styles.css': 'scss/index.scss',
+				},
+			},
+		},
+		ts: {
+			default: {
+				tsconfig: './TSConfig.json'
+			}
+		},
+		browserify: {
+			dist: {
+				files: {
+					'build/ui.js': ['src/ui.js'],
+					'build/preload.js': ['src/preload.js'],
+				},
+			},
+		},
+		mochaTest: {
+			test: {
+				src: ['test/**/*.js']
+			}
+		}
+	});
+	
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-ts');
+	
+	grunt.registerTask('build', ['clean', 'copy', 'sass', 'ts', 'browserify']);
+	grunt.registerTask('test', ['mochaTest']);
+	
+};
